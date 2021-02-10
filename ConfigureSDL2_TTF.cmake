c52d72709afef955e3784f9b255e8a64fd7377f8
JoseAguerre <74383347+JoseAguerre@users.noreply.github.com>
2021-02-10 17:14:06 -0300

    First commit

---
+++ ConfigureSDL2_TTF.cmake
@@ -0,0 +1,13 @@
+if(WIN32)
+	SET(SDL2TTF_INCLUDE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_ttf/include")
+	SET(SDL2TTF_LIBRARY "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_ttf/lib/SDL2_ttf.lib")
+	SET(SDL2TTF_RUNTIME_FILES "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_ttf/lib/SDL2_ttf.dll"
+							  "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_ttf/lib/libfreetype-6.dll"
+							  "${CMAKE_CURRENT_SOURCE_DIR}/SDL2_ttf/lib/zlib1.dll")
+	
+endif()
+if (APPLE)
+# for more info, see https://wolfgang-ziegler.com/blog/sdl-cmake-osx
+	find_package(SDLTTF REQUIRED)
+
+endif()
